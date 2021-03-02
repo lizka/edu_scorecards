704ea02c3c4f114fe1ae568731e13fb1b5536045
Eliza Krajewska <elizakrajewska@icloud.com>
2021-03-01 19:40:45 -0800

    Add typography module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './typography.css';
+import App from './components/app';
 
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
