7b044926ee6ea4ad6e7834c63d418648a2f8f5ee
Eliza Krajewska <elizakrajewska@icloud.com>
2021-03-01 21:12:55 -0800

    Update Search Bar Component

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -1,1 +1,23 @@
+import React from 'react';
 
+import './search-bar.css';
+
+class SearchBar extends React.Component {
+    state = { query: '' }
+
+    onFormChange = event => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.query);
+    }
+
+    render() {
+        return (
+            <form className="notion-search-bar__container" onChange={this.onFormChange} onSubmit={this.onFormChange}>
+                <input className="notion-search-bar" type="text" value={this.state.query} placeholder="Search..." onChange={(e) => this.setState({ query: e.target.value }) } />
+            </form>
+        );
+    }
+}
+
+export default SearchBar;
