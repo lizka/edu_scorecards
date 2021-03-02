111f306d5efd4876e4c6057f93f1933343a3a571
Eliza Krajewska <elizakrajewska@icloud.com>
2021-03-01 20:19:49 -0800

    Update Hero component

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,1 +1,24 @@
+import React from 'react';
 
+import './hero.css';
+import prince from './prince.svg';
+
+class Hero extends React.Component {
+    render() {
+        return (
+            <section class="notion-media">
+                <section class="notion-media__container">
+                    <section className="notion-media__body">
+                        <h1 className="primary-text__header">Find the university that’s right for you.</h1>
+                        <h2 className="secondary-text__header">Tenetur ex explicabo et illo. Recusandae fugit eius voluptatem. Voluptas atque autem totam.</h2>
+                    </section>
+                    <aside>
+                        <img src={prince} className="notion-media__img" alt="The Little Prince" />
+                    </aside>
+                </section>
+            </section>
+        );
+    }
+}
+
+export default Hero;
