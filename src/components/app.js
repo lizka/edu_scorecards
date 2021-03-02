5f0df9fc1c12b1c85cbd5c3473b0bfeac33c12e3
Eliza Krajewska <elizakrajewska@icloud.com>
2021-03-01 20:02:43 -0800

    Fetch and parse data from CSV file

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import * as d3 from 'd3';
+
+import scoreCardFile from '../mocks/edu-scorecard.csv';
 
 class App extends React.Component {
+    state = {
+        cards: [],
+        query: ''
+    };
+
+    async componentDidMount() {
+        const response = await d3.csv(scoreCardFile);
+        this.setState({
+            cards: response
+        });
+        console.log(this.state.cards);
+    }
+
     render() {    
         return (
             <main className="ui container">
@@ -11,3 +27,4 @@ class App extends React.Component {
 };
 
 export default App;
+
