3f937ed6663e42bb963b80ab7dd86a7d96e90129
Eliza Krajewska <elizakrajewska@icloud.com>
2021-03-01 19:42:03 -0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+class App extends React.Component {
+    render() {    
+        return (
+            <main className="ui container">
+               <h1>Hello Notion!</h1> 
+            </main>
+        );
+    }
+};
+
+export default App;
