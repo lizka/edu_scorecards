eca736eb854e18d30474b3ecb65b82300fe0bd41
Eliza Krajewska <elizakrajewska@icloud.com>
2021-03-01 20:48:44 -0800

    Update Header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,20 @@
+import React from 'react';
 
+import './header.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="notion__header">
+                <h1 className="notion__logo">YourUni</h1>
+                <ul className="notion__navigation" role="navigation">
+                    <li className="active">Product</li>
+                    <li>Download</li>
+                    <li>Pricing</li>
+                </ul>
+            </header>
+        );
+    }
+}
+
+export default Header;
