b7e04bd741dfb18dbc4f7310fa5aaa3566124067
Eliza Krajewska <elizakrajewska@icloud.com>
2021-03-01 21:14:30 -0800

    Invoke onSubmit event on parent component

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,11 @@ import * as d3 from 'd3';
 
 import scoreCardFile from '../mocks/edu-scorecard.csv';
 
+import Cards from './cards/cards';
 import Header from './header/header';
 import Hero from './hero/hero';
-import Cards from './cards/cards';
+import SearchBar from './search-bar/search-bar';
+
 
 class App extends React.Component {
     state = {
@@ -20,11 +22,17 @@ class App extends React.Component {
         });
     }
 
+    onSearchSubmit = (query) => {
+        // TODO - implement debouncing & unit tests
+        this.setState({ query: query });
+    }
+
     render() {    
         return (
             <main className="ui container">
                 <Header/>
                 <Hero />
+                <SearchBar onSubmit={this.onSearchSubmit} />
                 <Cards cards={this.state.cards} query={this.state.query}/>
             </main>
         );
